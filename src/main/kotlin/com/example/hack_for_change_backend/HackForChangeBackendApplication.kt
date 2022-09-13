3143b1f271178b166b4df4cd55720202d72bcd3d
Edward Todd <edwardtodd@Edwards-MacBook-Pro-M1.local>
2022-09-13 14:05:15 +0100

    First commit

---
+++ src/main/kotlin/com/example/hack_for_change_backend/HackForChangeBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hack_for_change_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HackForChangeBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<HackForChangeBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/hack_for_change_backend/HackForChangeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hack_for_change_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HackForChangeBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
