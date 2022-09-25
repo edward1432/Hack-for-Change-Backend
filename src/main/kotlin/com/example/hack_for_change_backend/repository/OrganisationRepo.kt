aeaa91ad33e3f0a5fa04b9caea243c4aece31d28
Alexander <alexander.leaver@me.com>
2022-09-25 11:34:38 +0100

    added some queries to organisation and user repo

--- src/main/kotlin/com/example/hack_for_change_backend/repository/OrganisationRepo.kt
+++ src/main/kotlin/com/example/hack_for_change_backend/repository/OrganisationRepo.kt
@@ -1,10 +1,20 @@
 package com.example.hack_for_change_backend.repository
 
 import com.example.hack_for_change_backend.model.Organisation
+import com.example.hack_for_change_backend.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface OrganisationRepo : JpaRepository<Organisation, Long> {
     companion object
+
+    @Query(value = "SELECT * FROM organisations WHERE id = ?", nativeQuery = true)
+    fun findOrganisationById(id: Long): Organisation
+
+    @Query(value = "SELECT * FROM organisations WHERE lower(organisation) LIKE lower(?1)", nativeQuery = true)
+    fun findOrganisationByName(@Param("organisations") organisations: String): List<Organisation>
+
 }
