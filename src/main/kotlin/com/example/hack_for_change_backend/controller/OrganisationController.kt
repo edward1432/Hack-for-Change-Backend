d8971942d29ef2b878998edf947d9a25cfada857
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-10-01 12:14:57 +0100

    Postgres configured and added CORS

--- src/main/kotlin/com/example/hack_for_change_backend/controller/OrganisationController.kt
+++ src/main/kotlin/com/example/hack_for_change_backend/controller/OrganisationController.kt
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:3000"])
 @RequestMapping("/organisations")
 class OrganisationController(private val organisationService: OrganisationService) {
 
