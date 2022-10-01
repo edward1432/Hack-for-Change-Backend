08bc6e1d1d1be354317109c2b7b03b27b7443a29
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-10-01 16:05:14 +0100

    Postgres configured and added CORS

--- src/main/kotlin/com/example/hack_for_change_backend/controller/EventEnumController.kt
+++ src/main/kotlin/com/example/hack_for_change_backend/controller/EventEnumController.kt
@@ -2,11 +2,13 @@ package com.example.hack_for_change_backend.controller
 
 import com.example.hack_for_change_backend.service.EventEnumService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:3000"])
 @RequestMapping("/eventEnums")
 class EventEnumController(private val eventEnumService: EventEnumService) {
 
