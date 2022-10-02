dcd0384b048924b17600de633da84c39766bca64
Edward Todd <edwardtdd@gmail.com>
2022-10-02 16:42:00 +0100

    a

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,18 +10,18 @@ insert into organisations (name, email, phone_no) values ('Brainlounge', 'oolenc
 insert into organisations (name, email, phone_no) values ('Lazz', 'gstatefield8@princeton.edu', '909171,2970');
 insert into organisations (name, email, phone_no) values ('Wordify', 'dvanderkruys9@army.mil', '143549,9000');
 
-INSERT INTO events (location, start_date_time, end_date_time, organisation_id, event_type) VALUES ('London', '2022-10-01 19:00:00', '2022-10-01 23:00:00', 1, 3);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Twitterbridge', '04-19-2023 19:00:00', '04-19-2023 23:00:00', 10, 0);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Lajo', '03-13-2023 19:00:00', '03-13-2023 23:00:00', 6, 4);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Jaxspan', '12-28-2022 19:00:00', '12-28-2022 23:00:00', 8, 3);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Flipstorm', '12-26-2022 19:00:00', '12-26-2022 23:00:00', 7, 1);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Abatz', '02-18-2023 19:00:00', '02-18-2023 23:00:00', 7, 5);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Eadel', '11-06-2022 19:00:00', '11-06-2022 23:00:00', 4, 5);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Mynte', '05-27-2023 19:00:00', '05-27-2023 23:00:00', 7, 5);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Zoomdog', '12-07-2022 19:00:00', '12-07-2022 23:00:00', 1, 1);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Flipopia', '03-28-2023 19:00:00', '03-28-2023 23:00:00', 10, 2);
---insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Livefish', '11-14-2022 19:00:00', '11-14-2022 23:00:00', 1, 6);
+INSERT INTO events (location, start_date_time, end_date_time, organisation_id, event_type) VALUES ('London', '2022-10-01 19:00:00', '2022-10-01 23:00:00', 1, 'DRINKING');
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Twitterbridge', '04-19-2023 19:00:00', '04-19-2023 23:00:00', 10, 0);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Lajo', '03-13-2023 19:00:00', '03-13-2023 23:00:00', 6, 4);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Jaxspan', '12-28-2022 19:00:00', '12-28-2022 23:00:00', 8, 3);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Flipstorm', '12-26-2022 19:00:00', '12-26-2022 23:00:00', 7, 1);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Abatz', '02-18-2023 19:00:00', '02-18-2023 23:00:00', 7, 5);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Eadel', '11-06-2022 19:00:00', '11-06-2022 23:00:00', 4, 5);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Mynte', '05-27-2023 19:00:00', '05-27-2023 23:00:00', 7, 5);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Zoomdog', '12-07-2022 19:00:00', '12-07-2022 23:00:00', 1, 1);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Flipopia', '03-28-2023 19:00:00', '03-28-2023 23:00:00', 10, 2);
+insert into events (location, start_date_time, end_date_time, organisation_id, event_type) values ('Livefish', '11-14-2022 19:00:00', '11-14-2022 23:00:00', 1, 6);
 
 INSERT INTO employees (user_name, organisation_id) VALUES ('employee1', 1);
 
-INSERT INTO venues (location, name, type) VALUES ('London', 'Slug and Lettuce', 'RESTAURANT');
+INSERT INTO venues (location, name, type) VALUES ('London', 'Slug and Lettuce', 1);
