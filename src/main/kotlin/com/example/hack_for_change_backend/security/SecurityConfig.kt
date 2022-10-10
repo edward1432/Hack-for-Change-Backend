f1177612361c704ee9cc5ae6098553b910ad803a
Alex <alexander.leaver@me.com>
2022-10-10 16:30:09 +0100

    jwt pom

--- src/main/kotlin/com/example/hack_for_change_backend/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/hack_for_change_backend/security/SecurityConfig.kt
@@ -1,45 +1,82 @@
-package com.example.hack_for_change_backend.security
+import com.nepooomuk.account.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
+import org.springframework.context.annotation.Primary
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices
+import org.springframework.security.oauth2.provider.token.TokenStore
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
 
 @Configuration
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+open class SecurityConfig @Autowired
+constructor(private val userService: UserService) : WebSecurityConfigurerAdapter() {
+
+    @Value("\${security.signing-key}")
+    private val signingKey: String? = null
+
+    @Value("\${security.encoding-strength}")
+    private val encodingStrength: Int? = null
+
+    @Value("\${security.security-realm}")
+    private val securityRealm: String? = null
 
     @Bean
-    fun encoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
+    @Throws(Exception::class)
+    override fun authenticationManager(): AuthenticationManager {
+        return super.authenticationManager()
     }
 
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication()
-            .withUser("alex1")
-            .password(encoder().encode("admin"))
-            .roles("ADMIN")
-            .and()
-            .withUser("edward1")
-            .password(encoder().encode("admin"))
-            .roles("ADMIN")
+    @Throws(Exception::class)
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth!!.userDetailsService(userService)
+            .passwordEncoder(BCryptPasswordEncoder())
     }
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.httpBasic()
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-            .authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/enjoyer").hasRole("ADMIN")
-            .antMatchers(HttpMethod.POST, "/enjoyer/**").hasRole("ADMIN")
-            .antMatchers(HttpMethod.PUT, "/enjoyer/**").hasRole("ADMIN")
-            .antMatchers(HttpMethod.DELETE, "/enjoyer/**").hasRole("ADMIN")
-            .antMatchers(HttpMethod.GET, "/enjoyer/**").hasAnyRole("ADMIN", )
+            .httpBasic()
+            .realmName(securityRealm)
             .and()
-            .csrf().disable()
-            .formLogin().disable()
+            .csrf()
+            .disable()
+
     }
 
+    @Bean
+    open fun accessTokenConverter(): JwtAccessTokenConverter {
+        val converter = JwtAccessTokenConverter()
+        converter.setSigningKey(signingKey!!)
+        return converter
+    }
+
+    @Bean
+    open fun tokenStore(): TokenStore {
+        return JwtTokenStore(accessTokenConverter())
+    }
+
+    @Bean
+    @Primary
+    //Making this primary to avoid any accidental duplication with another token service instance of the same name
+    open fun tokenServices(): DefaultTokenServices {
+        val defaultTokenServices = DefaultTokenServices()
+        defaultTokenServices.setTokenStore(tokenStore())
+        defaultTokenServices.setSupportRefreshToken(true)
+        return defaultTokenServices
+    }
 }
