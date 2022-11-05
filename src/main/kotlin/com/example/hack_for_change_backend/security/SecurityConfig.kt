e50a6f8b0aa4aa91f484bb7388ffd9be41f9f5a7
Edward Todd <edwardtdd@gmail.com>
2022-11-05 11:52:16 +0000

    Changed security config

--- src/main/kotlin/com/example/hack_for_change_backend/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/hack_for_change_backend/security/SecurityConfig.kt
@@ -2,7 +2,7 @@ package com.example.hack_for_change_backend.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -11,6 +11,32 @@ import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/v*/registration/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated().and()
+            .formLogin()
+    }
+
+//    @Throws(Exception::class)
+//    override fun configure(auth: AuthenticationManagerBuilder) {
+//        auth.authenticationProvider(daoAuthenticationProvider())
+//    }
+//
+//    @Bean
+//    fun daoAuthenticationProvider(): DaoAuthenticationProvider? {
+//        val provider = DaoAuthenticationProvider()
+//        provider.setPasswordEncoder(bCryptPasswordEncoder)
+//        provider.setUserDetailsService(appUserService)
+//        return provider
+//    }
+
 
     @Bean
     fun encoder(): PasswordEncoder {
@@ -27,22 +53,22 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             .password(encoder().encode("user"))
             .roles("USER")
     }
-
-    @Throws(Exception::class)
-    @Override
-    override fun configure(http: HttpSecurity) {
-        http.httpBasic()
-            .and()
-            .authorizeRequests()
-//            .antMatchers(HttpMethod.GET, "/").hasRole("ADMIN")
-            .antMatchers("/users/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/users/addUser/**").hasAnyAuthority("ADMIN", "USER")
-//            .antMatchers(HttpMethod.PUT, "//**").hasRole("ADMIN")
-//            .antMatchers(HttpMethod.DELETE, "//**").hasRole("ADMIN")
-//            .antMatchers(HttpMethod.GET, "//**").hasAnyRole("ADMIN", "USER")
-            .and()
-            .csrf().disable()
-            .formLogin().disable()
-    }
+//
+//    @Throws(Exception::class)
+//    @Override
+//    override fun configure(http: HttpSecurity) {
+//        http.httpBasic()
+//            .and()
+//            .authorizeRequests()
+////            .antMatchers(HttpMethod.GET, "/").hasRole("ADMIN")
+//            .antMatchers("/users/**").permitAll()
+//            .antMatchers(HttpMethod.POST, "/users/addUser/**").hasAnyAuthority("ADMIN", "USER")
+////            .antMatchers(HttpMethod.PUT, "//**").hasRole("ADMIN")
+////            .antMatchers(HttpMethod.DELETE, "//**").hasRole("ADMIN")
+////            .antMatchers(HttpMethod.GET, "//**").hasAnyRole("ADMIN", "USER")
+//            .and()
+//            .csrf().disable()
+//            .formLogin().disable()
+//    }
 
 }
